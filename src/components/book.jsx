8e02821b6885ae88f49089a4bbde8fd830b23d6a
eddyjav <eddy.j.av@gmail.com>
2023-01-07 16:41:45 -0500

    :books: add component/book & modified index

---
+++ src/components/book.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const Book = ({ item }) => {
+  return (
+    <div>
+      <Link to={`/view/${item.id}`}>
+        <img src={item.cover} width="200" alt={item.title} />
+        <div>{item.title}</div>
+      </Link>
+    </div>
+  );
+};
+
+export default Book;
