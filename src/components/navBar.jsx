53d70d4472b0c7ecadd89644eb47941034be150b
eddyjav <eddy.j.av@gmail.com>
2023-01-08 18:28:19 -0500

    :books: add styled init routes modif

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,12 +1,22 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/create">Create</Link>
-      <Link to="/"></Link>
-    </div>
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+      <div className="navbar-collapse">
+        <div className="navbar-nav">
+          <Link className="navbar-brand" to="/">
+            Book library
+          </Link>
+          <NavLink className="isActive nav-item nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="isActive nav-item nav-link" to="/create">
+            Create
+          </NavLink>
+        </div>
+      </div>
+    </nav>
   );
 };
 
